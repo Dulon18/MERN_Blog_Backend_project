ddda83c535f790fbaf402cb483f015884777b962
Dulon <dulon.iubat@gmail.com>
2024-04-23 11:52:05 -0700

    user route

---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+const User = require('./models/User');
+
+// Example route for creating a new user
+app.post('/api/users', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+        res.status(201).json(newUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
